8ac1e37efd1e38f4d1189b59ccea966bcbd438bb
Oleksiy Pyltsov <valar999@gmail.com>
2022-10-06 19:33:44 +0200

    Change neon-tests.sh

--- neon-tests.sh
+++ neon-tests.sh
@@ -5,8 +5,10 @@ set -e
 wget -O docker-compose-test.yml https://raw.githubusercontent.com/neonlabsorg/proxy-model.py/develop/proxy/docker-compose-test.yml
 
 # Run Neon
-REVISION=v0.12.0 NEON_EVM_COMMIT=v0.12.0 FAUCET_COMMIT=v0.12.0 \
-  docker-compose -f docker-compose-test.yml up -d --quiet-pull
+export REVISION=v0.12.0
+export NEON_EVM_COMMIT=v0.12.0
+export FAUCET_COMMIT=v0.12.0
+docker-compose -f docker-compose-test.yml up -d --quiet-pull
 
 # Run tests
 MNEMONIC_PHRASE="slide clip fancy range predict resource stuff once all insect sniff acid"
