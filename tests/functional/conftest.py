dd3933d085867b4aae4101d7565ba8267fbf87f5
kristinaNikolaeva <kn@neonlabs.org>
2023-05-25 17:43:21 +0300

    fixed fixtures scope

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -3,30 +3,30 @@ import pytest
 from brownie import Token, TokenNoReturn
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def gov(accounts):
     yield accounts[0]
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def rewards(accounts):
     yield accounts[1]
 
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def guardian(accounts):
     yield accounts[2]
 
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def management(accounts):
     yield accounts[3]
 
 
 
-@pytest.fixture
+@pytest.fixture(scope="module")
 def create_token(gov):
     def create_token(decimal=18, behaviour="Normal"):
         assert behaviour in ("Normal", "NoReturn")
@@ -35,14 +35,14 @@ def create_token(gov):
     yield create_token
 
 
-@pytest.fixture(params=[("Normal", 18), ("NoReturn", 18), ("Normal", 8), ("Normal", 2)])
-def token(create_token, request):
+@pytest.fixture(scope="module")#(params=[("Normal", 18), ("NoReturn", 18), ("Normal", 8), ("Normal", 2)])
+def token(create_token):
     # NOTE: Run our test suite using both compliant and non-compliant ERC20 Token
-    (behaviour, decimal) = request.param
+    (behaviour, decimal) = ("Normal", 18)
     yield create_token(decimal=decimal, behaviour=behaviour)
 
 
-@pytest.fixture
+@pytest.fixture(scope="module")
 def create_vault(gov, guardian, rewards, create_token, patch_vault_version):
     def create_vault(token=None, version=None, governance=gov):
         if token is None:
@@ -55,7 +55,7 @@ def create_vault(gov, guardian, rewards, create_token, patch_vault_version):
     yield create_vault
 
 
-@pytest.fixture
+@pytest.fixture(scope="module")
 def vault(gov, management, token, create_vault):
     vault = create_vault(token=token, governance=gov)
     vault.setManagement(management, {"from": gov})
@@ -66,18 +66,18 @@ def vault(gov, management, token, create_vault):
     yield vault
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def strategist(accounts):
     yield accounts[4]
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def keeper(accounts):
     yield accounts[5]
 
 
 
-@pytest.fixture(params=["RegularStrategy", "ClonedStrategy"])
+@pytest.fixture(scope="module", params=["RegularStrategy", "ClonedStrategy"])
 def strategy(gov, strategist, keeper, rewards, vault, TestStrategy, request):
     strategy = strategist.deploy(TestStrategy, vault)
 
